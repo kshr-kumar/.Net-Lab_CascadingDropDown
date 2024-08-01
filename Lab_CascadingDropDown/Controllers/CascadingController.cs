ac21986162557b6cf6b833ec61f5dba23b72588e
kishore <kishore@WIN-10>
2024-08-01 12:49:01 +0000

    Add project files.

---
+++ Lab_CascadingDropDown/Controllers/CascadingController.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CascadingController : Controller
+    {
+
+        AppDbContext _db;
+        public CascadingController(AppDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Country() 
+        {
+            var co = _db.Countries.ToList();
+            return new JsonResult(co); 
+        }
+        public JsonResult City(int id) 
+        {
+            var ci = _db.Cities.Where(e => e.Country.CountryId == id).ToList(); 
+            return new JsonResult(ci); 
+        }
+    }
+}
